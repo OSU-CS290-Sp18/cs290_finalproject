e10d567f4cee7e7d3bbb7391ff291d05542203c4
Hunter Lannon <hunter.d.lannon@gmail.com>
2018-05-30 22:34:49 -0700

    Switched serving stack to Express

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ const path = require('path');
 const url = require('url');
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+var express = require('express');
+var app = express(); 
+var router = express.Router({
+	"caseSensitive" : true
+});
+
+app.use('/', express.static('./bookshelf'));
 
 //CHANGE THIS TO classmongo
 var host = "localhost";
@@ -31,28 +38,32 @@ console.log("using port: ", port);
 
 var mongourl = "mongodb://" + host + ":27017";
 
-function getObjects(queryObjects, collectionName, callback){
+function getObjects(req, res, queryObjects, collectionName, callback){
 	var objects;
 	//array of objects
-	var allResults = [];
+	let allResults = new Array;
 	MongoClient.connect(mongourl, function(err, client){
 		var db = client.db(dbname);
 		var collection = db.collection(collectionName);
-		objects = [];
-		queryObjects.forEach( function (queryObject) {
+		queryObjects.forEach( function (queryObject, idx) {
 			collection.find(queryObject).limit(10).toArray(function(err, results){
 				if(err){
 					console.log("Error finding documents");
 					callback(false);
+				}else{
+					console.log("THE QUERY ITSELF", queryObject);
+					console.log("RESULTS OF QUERY: ", results);
+					results.forEach(function (element){
+						allResults.push(element);
+					});
+					callback(allResults);
+				}		
+				if (idx === queryObjects.length - 1){
+					res.end();
 				}
-				console.log("THE QUERY ITSELF", queryObject);
-				console.log("RESULTS OF QUERY: ", results);
-				console.log("TYPE OF QUERY: ", typeof(results));
-				allResults.push(results);
 			});
 		});
 	});
-	callback(allResults);
 }
 
 var shitty_words = ["an", "the", "a", "in", "is"];
@@ -69,7 +80,9 @@ function createQuery(str){
 		var matchers = substr.split("=")[1].split("+");
 		console.log("Matchers: ", matchers);
 		matchers.forEach ( function (word) {
-			regex += (word + "|");
+			if(!shitty_words.includes(word)){
+				regex += (word + "|");
+			}	
 		});
 		
 		regex = regex.slice(0, regex.length - 1);
@@ -81,71 +94,73 @@ function createQuery(str){
 	return queries;
 }
 
-function requestHandler(req, res){
-	//assume status of 200 until something goes wrong
-	stat = 200;
+function getCollection(query){
+	var startindex = query.includes("collection=");
+	if( startindex ){
+		startindex += "collection=".length - 1;
+		let endindex = query.indexOf("&", startindex);
+		if(endindex == -1){
+			endindex = query.length;
+		}
+		var cname = query.slice(startindex, endindex);
+		console.log("REQUEST FOR COLLECTION: ", cname);
+		return cname;
+	}
+	else{
+		return "bookshelf"
+	}
+}
 
-	console.log("Request Made");
-
-	//get file type/extension without the period
+function getFileContents(req, res, filepath){
 	var type = path.extname(req.url).replace(/\./, "");
 	if(!type){
 		type ="html";
 	}
-	
-	//set relative filepath. Set it to bookshelf/index.html if no page is specified
-	if(req.url == "/"){
-		var filepath = path_base + default_file;
-	}else{
-		var filepath = path_base + req.url.replace(/\/\?.*/, "");
-	}
-
-	var urlObject = new url.parse(req.url, false);
-
-	console.log("query:", urlObject.query);
-	console.log("Parsed resource path:", filepath);
-	if(urlObject.query){
-		res.writeHead(stat, {'Content-Type': 'application/json'});
-		getObjects(createQuery(urlObject.query), "bookshelf", function(results){
-			//send back the results of database query
-			results.forEach( function (element) {
-				res.write(JSON.stringify(element));
-			});
-			res.end();
+	if(!(filepath in cache) ){
+		//print useful information
+		fs.readFile(filepath, function(err, data) {
+			//print error code 
+			if(err){console.log(err.code);}
+			
+			//send 404.html if file requested isn't on FS
+			if (err && (err.code == "ENOENT" || err.code == "ENODIR")) {
+				res.writeHead(404, {'Content-Type': 'text/html'});
+				return cache['404.html'];
+			}else{ //read file, store it in cache, and send it over the wire
+				console.log("READ FILE => ", filepath,  data);
+				cache[filepath] = data;
+				res.writeHead(200, {'Content-Type': 'text/' + type});
+				return data;
+			}
 		});
 	}else{
-		
-		//form path to file. defaults to index.html
-		//Read file if not in cache
-		if(!(filepath in cache) ){
-			//print useful information
-			console.log(`received request for ${req.url}`);
-			console.log(`received request for ${type} file extension`);
-			fs.readFile(filepath, function(err, data) {
-				//print error code 
-				if(err){console.log(err.code);}
-				
-				//send 404.html if file requested isn't on FS
-				if (err && (err.code == "ENOENT" || err.code == "ENODIR")) {
-					stat = 404;
-					res.writeHead(stat, {'Content-Type': 'text/html'});
-					console.log("404 Page: " , cache['404.html'])
-					res.end(cache['404.html']);
-				}else{ //read file, store it in cache, and send it over the wire
-					console.log("READ FILE => ", data);
-					cache[filepath] = data;
-					res.writeHead(stat, {'Content-Type': 'text/' + type});
-					res.end(data);
-				}
-			});
-		}else{
-			//send cached file with 200 status
-			res.writeHead(stat, {'Content-Type': 'text/' + type});
-			res.end(cache[filepath]);
-		}
+		res.writeHead(200, {'Content-Type': 'text/' + type});
+		return cache[filepath];
 	}
 }
 
+app.get('/', (req, res) => res.sendFile(__dirname + "/bookshelf/index.html"));
+app.get('/query', function (req, res) {
+	console.log("Parsed resource path:", req.filepath);
+	var urlObject = new url.parse(req.url, false);
+	console.log("query:", urlObject.query);
+	console.log("URL OBJECT:", urlObject);
+	if(urlObject.query){
+		res.writeHead(200, {'Content-Type': 'application/json'});
+		getObjects(req, res, createQuery(urlObject.query), getCollection(urlObject.query), function(results){
+			//send back the results of database query
+			results.forEach( function (element) {
+				console.log("Sending Object: ", element);
+				res.write(JSON.stringify(element));
+			});
+		});
+	}
+});
 
-var server = http.createServer(requestHandler);
-server.listen(port);
+app.get('/*.html', (req, res) => res.sendFile(__dirname + "/bookshelf/html" + req.path));
+app.get('/*.css', (req, res) => res.sendFile(__dirname + "/bookshelf/css" + req.path));
+app.get('/*.js', (req, res) => res.sendFile(__dirname + "/bookshelf/scripts" + req.path));
+app.get('/*.jpg', (req, res) => res.sendFile(__dirname + "/bookshelf/images" + req.path));
+app.get('/*.png', (req, res) => res.sendFile(__dirname + "/bookshelf/images" + req.path));
+
+app.listen(port);
